c34e305901bf8254a4420056fc0efa5d44c2a3ad
Karen Croston <karencroston@hotmail.co.uk>
2023-03-19 15:22:23 +0000

    add auto escape

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -2,11 +2,11 @@ const createArtist = async (req, res) => {
     const { name, genre } = req.body
   
     try {
-      const { rows: [ artist ] } = await db.query(`INSERT INTO Artists (name, genre) VALUES ('${name}', '${genre}') RETURNING *`)
-      res.status(201).json(artist)
-    } catch (err) {
-      res.status(500).json(err.message)
+        const { rows: [ artist ] } = await db.query('INSERT INTO Artists (name, genre) VALUES ($1, $2) RETURNING *', [name, genre])
+        res.status(201).json(artist)
+      } catch (err) {
+        res.status(500).json(err.message)
+      }
     }
-  }
   
   module.exports = { createArtist }
